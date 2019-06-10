3f298241680e19dd129b5b2dbf901f66f8d481fc
David Arutiunian <arutunjan666@mail.com>
2019-06-10 19:22:13 +0300

    build(template-editor): enhance build process

--- packages/tempalte-editor/rollup.config.js
+++ packages/tempalte-editor/rollup.config.js
@@ -2,28 +2,47 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 
 const pkg = require("./package.json");
 
 export default {
     input: "src/index.ts",
-    output: [{ file: pkg.main, format: "cjs", sourcemap: true }],
-    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+            sourcemap: true,
+        },
+    ],
     external: Object.keys(pkg.peerDependencies),
-    watch: { include: "src/**" },
+    watch: {
+        include: "src/**",
+    },
+    treeshake: process.env.NODE_ENV === "production",
     plugins: [
-        // Compile TypeScript files
         typescript({
             rollupCommonJSResolveHack: true,
             cacheRoot: "./node_modules/.rts2_cache",
         }),
-        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-        commonjs({ include: /node_modules/ }),
-        // Allow node_modules resolution, so you can use 'external' to control
-        // which external modules to include in the bundle
-        // https://github.com/rollup/rollup-plugin-node-resolve#usage
-        resolve(),
-        // Resolve source maps to the original source
+        commonjs({
+            include: /node_modules/,
+        }),
+        resolve({
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+        }),
         sourcemaps(),
+        sizeSnapshot({
+            snapshotPath: "./node_modules/.size-snapshot.json",
+        }),
+        process.env.NODE_ENV === "production" &&
+            terser({
+                toplevel: true,
+            }),
     ],
+    onwarn: message => {
+        if (/crypto/.test(message)) return;
+        console.error(message);
+    },
 };
