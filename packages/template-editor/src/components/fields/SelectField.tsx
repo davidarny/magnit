48edf9e8d1c3c3231b1dcd7f023de86525a8afd2
Maria Kanyushkova <mashakanyushkova@gmail.com>
2019-07-01 09:54:03 +0300

    кастомизирован selectField

--- packages/template-editor/src/components/fields/SelectField.tsx
+++ packages/template-editor/src/components/fields/SelectField.tsx
@@ -1,7 +1,7 @@
 import { SelectProps } from "@material-ui/core/Select";
 import { ETerminals } from "../../entities";
 import { jsx } from "@emotion/core";
-import { FormControl, Input, InputLabel, Select } from "@material-ui/core";
+import { FormControl, Input, Select } from "@material-ui/core";
 
 export const SelectField: React.FC<SelectProps> = ({
     children,
@@ -17,25 +17,36 @@ export const SelectField: React.FC<SelectProps> = ({
             css={theme => ({
                 minHeight: theme.spacing(6),
                 height: "100%",
+                borderRadius: 40,
+                border: "1px solid #DEE5EF",
+                ":hover, :active": {
+                    border: `1px solid ${theme.colors.blue}`,
+                },
                 input: {
                     fontFamily: "Roboto",
                     color: theme.colors.black,
+                    borderRadius: 40,
+                    height: "100%",
                 },
                 div: {
-                    minHeight: 20,
-                    ":before": {
-                        borderBottom: "1px solid rgba(0, 0, 0, 0.42)",
-                    },
-                    ":after": {
-                        borderBottom: `2px solid ${theme.colors.primary}`,
+                    minHeight: 36,
+                    borderRadius: 40,
+                    overflow: "hidden",
+                    ":before, :after": {
+                        borderBottom: "none !important",
                     },
                     div: {
                         fontFamily: "Roboto",
                         color: theme.colors.black,
-                        paddingTop: 11,
+                        height: "100%",
+                        div: {
+                            padding: "6px 15px",
+                            lineHeight: "36px",
+                        },
                     },
                     svg: {
-                        top: 20,
+                        top: 12,
+                        right: 10,
                     },
                 },
             })}
