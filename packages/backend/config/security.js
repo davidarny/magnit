89f43cde6a276c4d0c61eed681641562d6e90fa7
David Arutiunian <arutunjan666@mail.com>
2019-07-09 15:54:05 +0300

    build: allow cors

--- packages/backend/config/security.js
+++ packages/backend/config/security.js
@@ -26,11 +26,11 @@ module.exports.security = {
      * https://sailsjs.com/docs/concepts/security/cors                          *
      *                                                                          *
      ***************************************************************************/
-    // cors: {
-    //   allRoutes: false,
-    //   allowOrigins: '*',
-    //   allowCredentials: false,
-    // },
+    cors: {
+        allRoutes: true,
+        allowOrigins: "*",
+        allowCredentials: false,
+    },
     /****************************************************************************
      *                                                                           *
      * By default, Sails' built-in CSRF protection is disabled to facilitate     *
