013865205c0324929a4e680c939d5b529857af3a
David Arutiunian <arutunjan666@mail.com>
2019-07-11 15:14:45 +0300

    fix(update template api): convert to snake_case on update

--- packages/frontend/src/services/api/templates/UpdateTemplate.ts
+++ packages/frontend/src/services/api/templates/UpdateTemplate.ts
@@ -1,5 +1,18 @@
 import { ICourier, IResponse } from "services/api";
+import _ from "lodash";
+import { traverse } from "@magnit/template-editor";
+import { toSnakeCase } from "services/string";
 
 export async function updateTemplate(courier: ICourier, id: number, template: object) {
-    return courier.put<IResponse>(`templates/${id}`, { template });
+    const buffer = _.cloneDeep(template);
+    // convert all props to snake_case before saving
+    traverse(buffer, (object: any) => {
+        if (_.isObject(object)) {
+            _.mapKeys(object, (value: any, key: string) => {
+                delete (object as { [key: string]: any })[key];
+                (object as { [key: string]: any })[toSnakeCase(key)] = value;
+            });
+        }
+    });
+    return courier.put<IResponse>(`templates/${id}`, { template: buffer });
 }
