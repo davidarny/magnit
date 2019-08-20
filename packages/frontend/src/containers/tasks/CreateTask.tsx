0a980f5d841f9e97b056a875e965362e6bf3360a
David Arutiunian <arutunjan666@mail.com>
2019-08-20 22:48:37 +0300

    fix(frontend): fix creating task
    update @magnit/task-editor packages

--- packages/frontend/src/containers/tasks/CreateTask.tsx
+++ packages/frontend/src/containers/tasks/CreateTask.tsx
@@ -3,6 +3,7 @@
 import { jsx } from "@emotion/core";
 import { Button } from "@magnit/components";
 import { SendIcon } from "@magnit/icons";
+import { ETaskStatus } from "@magnit/services";
 import { ITask, TaskEditor } from "@magnit/task-editor";
 import { ITemplate } from "@magnit/template-editor";
 import { Grid, Typography } from "@material-ui/core";
@@ -28,6 +29,7 @@ export const CreateTask: React.FC = () => {
         title: "",
         id: uuid(),
         templates: [],
+        status: ETaskStatus.DRAFT,
     });
     const [redirect, setRedirect] = useState(false);
     const [error, setError] = useState(false); // success/error snackbar state
