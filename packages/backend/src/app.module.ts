672b42583d02b5034cd120754c36e8533b0255bc
David Arutiunian <arutunjan666@mail.com>
2019-08-30 13:12:50 +0300

    fix(backend): fix missing import

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { resolve } from "path";
 import { ConnectionOptionsReader } from "typeorm";
 import { AssetModule } from "./modules/asset/asset.module";
+import { AirwatchAuthModule } from "./modules/auth/airwatch.auth.module";
 import { MailModule } from "./modules/mail/mail.module";
 import { TaskModule } from "./modules/task/task.module";
 import { TemplateModule } from "./modules/template/template.module";
