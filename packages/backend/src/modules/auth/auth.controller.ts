09231665a25cbe7f0f95d6ed0ac785b8a8d181dd
Dmitriy Mamatov <dmitriy.mamatov@omega-r.com>
2019-09-20 16:18:19 +0300

    feat(auth module): start to add controller

--- packages/backend/src/modules/auth/auth.controller.ts
+++ packages/backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Body, Post } from "@nestjs/common";
+import { User } from "./entities/user.entity";
 
 @Controller("auth")
-export class AuthController {}
+export class AuthController {
+    @Post("/register")
+    async register(@Body() user: User) {
+        return;
+    }
+
+    @Post("/login")
+    async login(@Body() user: User) {
+        return;
+    }
+}
