695895f7d842482d9a6a34611832e7088f2916ad
Alexey Vedushev <alexey.vedushev@omega-r.com>
2019-09-25 10:37:39 +0300

    feat(auth): remove logs

--- packages/backend/src/modules/auth/services/auth.service.ts
+++ packages/backend/src/modules/auth/services/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable, Inject } from "@nestjs/common";
 
 import { User } from "../entities/user.entity";
 import { UserService } from "./user.service";
-import { JwtTokenManager } from "../providers/jwt.token.manager";
 import { PasswordManager } from "../providers/password.manager";
 
 @Injectable()
@@ -14,12 +13,6 @@ export class AuthService {
 
     async validateUser(email: string, pass: string) {
         const user = await this.userService.findOneByEmail(email);
-        const encryptPassword = this.passwordManager.encode(pass);
-        console.log(`encryptPassword = ${encryptPassword} `);
-        console.log(`user password = ${user.password}`);
-        console.log(`user decode password = ${this.passwordManager.decode(user.password)}`);
-        console.log(`user =`);
-        console.log(user);
 
         if (user && pass === this.passwordManager.decode(user.password)) {
             const { password, ...result } = user;
