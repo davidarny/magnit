a0281cc2afc2956300891b635600a2d66382ed62
David Arutiunian <arutunjan666@mail.com>
2019-10-18 21:16:24 +0300

    feat(auth): better dto namings

--- packages/backend/src/modules/auth/airwatch.auth.controller.ts
+++ packages/backend/src/modules/auth/airwatch.auth.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Inject, Post } from "@nestjs/common";
 import { ApiOkResponse, ApiUnauthorizedResponse, ApiUseTags } from "@nestjs/swagger";
 import { UserUnauthorizedException } from "../../shared/exceptions/user-unauthorized.exception";
 import { ErrorResponse } from "../../shared/responses/error.response";
-import { AuthDto } from "./dto/auth.dto";
+import { LoginUserDto } from "./dto/login-user.dto";
 import { User } from "./entities/user.entity";
 import { ITokenManager } from "./interfaces/token.manager.interface";
 import { JwtTokenManager } from "./providers/jwt.token.manager";
-import { AuthResponse } from "./reponses/auth.response";
+import { LoginUserResponse } from "./reponses/login-user.response";
 import { AirwatchAuthService } from "./services/airwatch-auth.service";
 
 @Controller("auth")
@@ -17,13 +17,13 @@ export class AirwatchAuthController {
         @Inject(JwtTokenManager) private readonly tokenManager: ITokenManager<User>,
     ) {}
 
-    @Post("/")
-    @ApiOkResponse({ description: "User successfully authorized", type: AuthResponse })
+    @Post("/login")
+    @ApiOkResponse({ description: "User successfully authorized", type: LoginUserResponse })
     @ApiUnauthorizedResponse({ description: "Cannot authorize user", type: ErrorResponse })
-    async auth(@Body() authDto: AuthDto) {
+    async login(@Body() loginUserDto: LoginUserDto) {
         const user = await this.airwatchAuthService.validateUser(
-            authDto.username,
-            authDto.password,
+            loginUserDto.username,
+            loginUserDto.password,
         );
         if (!user) {
             throw new UserUnauthorizedException("Cannot authorize user");
--- packages/backend/src/modules/auth/dto/login-user.dto.ts
+++ packages/backend/src/modules/auth/dto/login-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiModelProperty } from "@nestjs/swagger";
 
-export class AuthDto {
+export class LoginUserDto {
     @ApiModelProperty() readonly username: string;
     @ApiModelProperty() readonly password: string;
 }
