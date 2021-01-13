8a212b619e33be2426162431e4527b3254016a30
David Arutiunian <arutunjan666@mail.com>
2021-01-13 17:39:02 +0300

    Updates

---
+++ precache-manifest.a01f7f527ed74d3464b1a4631fbb4441.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b13a50d162e7805e640de00f163a379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "684a4c846a416123cb37",
+    "url": "/static/js/0.ea204859.chunk.js"
+  },
+  {
+    "revision": "48d35d34fe3e0ccf189f",
+    "url": "/static/js/10.9338c773.chunk.js"
+  },
+  {
+    "revision": "08d0e3b8f5cadf406b41",
+    "url": "/static/js/11.730adf01.chunk.js"
+  },
+  {
+    "revision": "126642e722f74bd076e3",
+    "url": "/static/js/12.3eaf5d32.chunk.js"
+  },
+  {
+    "revision": "0a655f86f0ad04b07dca",
+    "url": "/static/js/13.22293346.chunk.js"
+  },
+  {
+    "revision": "015a679243aa05bcf794",
+    "url": "/static/js/14.ae47b426.chunk.js"
+  },
+  {
+    "revision": "16ada482e03ebb2e618b",
+    "url": "/static/js/3.845461d6.chunk.js"
+  },
+  {
+    "revision": "350fea3b6e2c641c3a9742a705040a34",
+    "url": "/static/js/3.845461d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b125a0885c47e811d2ed",
+    "url": "/static/js/4.a93d4a99.chunk.js"
+  },
+  {
+    "revision": "481ab5f3a4179fd38ffa",
+    "url": "/static/js/5.68f55ed7.chunk.js"
+  },
+  {
+    "revision": "c3c898850ca885db4f36",
+    "url": "/static/js/6.930995f0.chunk.js"
+  },
+  {
+    "revision": "b1fd402ee248266a218f",
+    "url": "/static/js/7.8c7d1146.chunk.js"
+  },
+  {
+    "revision": "e1db76bbfe28c239ffbf",
+    "url": "/static/js/8.3f80e1ae.chunk.js"
+  },
+  {
+    "revision": "b4f9f1f5db4d0754bb69",
+    "url": "/static/js/9.b92c92ff.chunk.js"
+  },
+  {
+    "revision": "446a95d281734cdd596d",
+    "url": "/static/js/main.cc4ff203.chunk.js"
+  },
+  {
+    "revision": "81d77cc4fac1ac77133b",
+    "url": "/static/js/runtime-main.e369ef7c.js"
+  },
+  {
+    "revision": "0fae78ebe71d7c92b5714d985db8a7db",
+    "url": "/static/media/splash.0fae78eb.png"
+  }
+]);
